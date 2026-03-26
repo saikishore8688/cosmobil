bc42063b519b8517503316943990715dd138c266
saikishore8688 <saikishorebojanapu@gmail.com>
2026-03-26 12:26:44 +0530

    added my first file

---
+++ script.js
@@ -0,0 +1,12 @@
+function scrollToProjects() {
+  document.querySelector('.projects').scrollIntoView();
+}
+
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80 },
+    size: { value: 3 },
+    move: { speed: 2 },
+    line_linked: { enable: true }
+  }
+});
